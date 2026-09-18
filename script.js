6396d90191aa319019a40d75ed8d42263b3e060f
WilbertAllanMuljadi <wilbert.535250053@stu.untar.ac.id>
2026-09-19 01:01:31 +0700

    membuat html faq dan logika faq

--- script.js
+++ script.js
@@ -53,4 +53,64 @@ $(document).ready(function() {
             }
         }
     });
+
+    $('#send-button').click(function(){
+        let input = $('#user-input').val();
+
+        if(input == '1') {
+            $('.chat-box').append(
+                '<div class="user-message">1</div>' 
+            );
+            $('.chat-box').append(
+                '<div class="bot-message">Kami menggunakan biji kopi Robusta, Arabica, serta beberapa biji kopi pilihan dari berbagai daerah di Indonesia.</div>'
+            );
+        } 
+        
+        else if(input == '2') {
+            $('.chat-box').append(
+                '<div class="user-message">2</div>'
+            );
+            $('.chat-box').append(
+                '<div class="bot-message">Di setiap Cafe Nusantara, kami selalu menyediakan wifi gratis yang bisa digunakan oleh setiap pengunjung.</div>'
+            );
+        }
+
+        else if(input == '3') {
+            $('.chat-box').append (
+                '<div class="user-message">3</div>'
+            );
+            $('.chat-box').append (
+                '<div class="bot-message">Untuk saat ini Cafe Nusantara baru tersedia di Jl. Letjen S. Parman, Tomang, Kec Grogol Petamburan, Jakarta Barat</div>'
+            );
+        }
+
+        else if(input == '4') {
+            $('.chat-box').append (
+                '<div class="user-message">4</div>'
+            );
+            $('.chat-box').append (
+                '<div class="bot-message">Cafe buka dari pukul 09:00 - 21:00</div>'
+            );
+        }
+
+        else if(input == '5') {
+            $('.chat-box').append (
+                '<div class="user-message">5</div>'
+            );
+            $('.chat-box').append(
+                '<div class="bot-message">Bisa banget, untuk info lebih lanjut bisa menghubungi nomor ini +62 834-6236-7788</div>'
+            );
+        }
+
+        else {
+            $('.chat-box').append (
+                '<div class="user-message">' + input + '</div>'
+            );
+            $('.chat-box').append (
+                '<div class="bot-message">Silahkan masukkan pertanyaan yang ada di list.</div>'
+            );
+        }
+
+         $('#user-input').val('');
+    });
 });
